b12c4ff69e7e28350d2b6142cc1db301cbf34bfe
MoAlhaddad <mohamadalhaddad25@gmail.com>
2022-09-08 13:05:37 -0700

    added post, and put to controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,53 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+
+import { data, ReportType } from './data';
+import { v4 as uuid } from "uuid"; 
 
 
 @Controller('report/:type')
 export class AppController {
   @Get()
-  getAllVapeSaleReports() {
-    return [];
+  getAllVapeSaleReports( @Param('type') type: string) {
+    const reportType = type === "income" ? ReportType.INCOME : ReportType.EXPENSE
+    return data.report.filter((report) => report.type === reportType)
+    
   }
 
   @Get(':id')
-    getVapeSaleReportById(){
-      return {};
+    getVapeSaleReportById(
+     @Param('type') type : string,
+    @Param('id') id: string
+    ){
+        const reportType = type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+        return data.report.
+        filter((report) =>  report.type === reportType).find((report) => report.id === id);
+      
     }
 
     @Post()
-    createVapeSalesReport(){
-      return "Created"
-    }
+    createVapeSalesReport(
+   @Body()  {amount, source}: {
+        amount: number;
+        source: string
+      },
+      @Param('type') type: string,
+    ){
+      const newReport = {
+        id: uuid(),
+        source,
+        amount,
+        created_at: new Date(),
+        updated_at: new Date(),
+        type: type === 'income' ? ReportType.INCOME : ReportType.EXPENSE,
+      }
+    data.report.push(newReport);
+      return newReport;
+    
+}
 
+
+    
     @Put(':id')
     updateVapeSalesReport(){
         return "updated";
@@ -28,7 +58,7 @@ export class AppController {
         return 'deelete';
       }
     
-  }
+    }
 
   //http://localhost:3000/report/sales/saleid22
 
--- src/data.ts
+++ src/data.ts
@@ -1,37 +1,55 @@
 import { report } from "process";
 
-export const data = {
-    report: []
-}
-
-interface Data {
-report: [{
-    id: string,
-    source: string,
-    amount: number,
-    created_at: Date,
-    updated_at: Date;
-    type: "sales" | "expense"
-}]
-}
-
-enum ReportType {
+export enum ReportType {
     INCOME = "sales",
     EXPENSE = "expense"
 }
 
-data.report.push({
-    id: "uuid",
-    source: "Sales",
-    amount: 6400,
-    created_at: new Date(),
-    updated_at: new Date(),
-    type:ReportType.INCOME
-})
+export const data: Data = {
+    report: [
+        {
+            id: "uuid1",
+            source: "HCOW 6000 Puff",
+            amount: 6000,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.INCOME
+        },
+        {
+            id: "uuid2",
+            source: "HCOW 2500 Puff",
+            amount: 4000,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.INCOME
+        },
+        {
+            id: "uuid3",
+            source: "Hcow 5000 puff",
+            amount: 6000,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.INCOME
+        },
+        {
+            id: "uuid4",
+            source: "SmokeShop Cosignment Bonus",
+            amount: 16500,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.INCOME
+        }
+    ],
+};
 
+interface Data {
+    report: {
+        id: string;
+        source: string;
+        amount: number;
+        created_at: Date;
+        updated_at: Date;
+        type: ReportType;
+    }[];
+    }
 
-data.report.push('threethousand')
-
-data.report.push(1268)
-
-data.report.push(true)
